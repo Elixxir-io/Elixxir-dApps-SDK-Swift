0f80a52c7848daf0cf66f4b92e3121ee0a908a98
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-29 13:21:08 +0100

    Run tests on macOS with run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 set -e
+
+echo "\n\033[1;32m▶ Running tests on macOS...\033[0m"
+set -o pipefail && swift test 2>&1 | ./xcbeautify
+
+echo "\n\033[1;32m▶ Running tests on iOS Simulator...\033[0m"
 set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
