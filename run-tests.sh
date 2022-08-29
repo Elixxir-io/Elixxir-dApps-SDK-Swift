a8447657060bd389ec844216aaafcf23507edc5f
Dariusz Rybicki <dariusz@elixxir.io>
2022-08-29 14:02:08 +0100

    Update CI scripts

--- run-tests.sh
+++ run-tests.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = "ios" ]; then
+echo "\n\033[1;32m▶ Running tests on iOS Simulator...\033[0m"
+set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
 
-  echo "\n\033[1;32m▶ Running tests on iOS Simulator...\033[0m"
-  set -o pipefail && xcodebuild -scheme 'elixxir-dapps-sdk-swift-Package' -sdk iphonesimulator -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 13' test | ./xcbeautify
-
-elif [ "$1" = "macos" ]; then
-  
-  echo "\n\033[1;32m▶ Running tests on macOS...\033[0m"
-  set -o pipefail && swift test 2>&1 | ./xcbeautify
-
-else
-
-  echo "Invalid option. Usage:"
-  echo "  run-tests.sh ios   - Run tests on iOS Simulator"
-  echo "  run-tests.sh macos - Run tests on macOS"
-  exit 1
-
-fi
+echo "\n\033[1;32m▶ Running tests on macOS...\033[0m"
+set -o pipefail && swift test 2>&1 | ./xcbeautify
